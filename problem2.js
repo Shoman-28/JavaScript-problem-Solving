cf80ec498d4f1c784d528f5df284f50efd258046
shoman <shomanmiah28@gmail.com>
2023-07-24 11:19:32 +0600

    first commit

---
+++ problem2.js
@@ -0,0 +1,84 @@
+// problem - 1
+
+// let harryMomGiv = 1000;
+// let orange = 400;
+// let apple = 300;
+// let totalCost = orange + apple;
+// console.log(totalCost)
+// let saveMany = harryMomGiv - totalCost;
+// console.log(saveMany)
+
+
+// problem 2
+
+// let mathematics = 75.25;
+// let biology = 65;
+// let chemistry = 80;
+// let physics = 35.45;
+// let bangla = 99.50;
+// let totalMark = mathematics + biology + chemistry + physics + bangla;
+// console.log(totalMark)
+// let avaregMark = totalMark / 5;
+// console.log(avaregMark.toFixed(2));
+
+
+// problem 3
+
+// let jon = 'I am going to be';
+// let jon2 = 'an awesome web developer';
+// console.log(jon, jon2)
+
+
+// problem 4
+
+// var fruits = ['Apple', 'Banana', 'Orange'];
+// var index = fruits.indexOf('Banana')
+// console.log(index);
+// fruits[1] = 'Mango';
+// console.log(fruits);
+// fruits.pop()
+// fruits.push("watermelon")
+// console.log(fruits);
+
+
+// problem 5
+
+
+// let number = 150;
+
+// if (80 <= number) {
+//   result = 'A grade';
+//   console.log(result)
+// } else if (60 <= number) {
+//   result = 'B grade';
+//   console.log(result)
+// }
+// else if (50 <= number) {
+//   result = 'C grade';
+//   console.log(result)
+// }
+// else if (40 <= number) {
+//   result = 'C grade';
+//   console.log(result)
+// } else {
+//   console.log('F grade')
+// }
+
+
+// problem 6
+
+
+// let number1 = 13;
+// let number2 = 79;
+// let number3 = 45;
+// if (number1 <= number2) {
+//   console.log("79 is Grade Number")
+// } else if (number2 <= number3) {
+//   console.log("45 is Grade Number")
+// } else {
+
+//   console.log("13 is grad number")
+// }
+
+
+// problem 7
